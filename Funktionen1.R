244d9b83b3384153f34815383d2e012cc7fa9250
Ahmgh123 <124621683+Ahmgh123@users.noreply.github.com>
2024-02-24 05:04:28 +0100

    Update Funktionen1.R

--- Funktionen1.R
+++ Funktionen1.R
@@ -46,7 +46,42 @@ deskriptive_Statistiken(data)
 
 ###############################################################################
 # 2a) (ii) - Deskriptive Stat. (kategorial):
+deskriptive_Statistiken_kateg = function (daten)
+{
+    # Überprüfen, ob daten ein DataFrame ist 
+    if (!is.data.frame(daten))
+    {
+        stop(  "Die übergebenen Daten sind kein DataFrame.")
+    }
+    # Überprüfen, ob der DataFrame mindestens eine kategoriale Variable (Faktor) enthält
+    if  (!any(sapply(daten, is.factor)))
+    {
+        stop("Der DataFrame enthält keine kategoriale Variable.")
+    }
 
+      # Selektieren aller kategorialen Variablen
+    kategoriale_variablen <- daten[, sapply(daten, is.factor)]
+
+    # Zusammenführen aller kategorialen Variablen in eine einzige Vektor
+    gesamte_kategorien <- unlist(kategoriale_variablen, use.names = FALSE)
+
+    # Berechnen der Gesamthäufigkeiten und relativen Häufigkeiten
+    abs_ha_gesamt <- table(gesamte_kategorien, useNA = "always")
+    re_ha_gesamt <- prop.table(abs_ha_gesamt)
+
+    # Erstellen eines DataFrames für die gesamten Statistiken
+    gesamte_statistiken <- data.frame(
+        Kategorie = names(abs_ha_gesamt),
+        Abs_ha = as.integer(abs_ha_gesamt),
+        Re_ha = re_ha_gesamt
+    )
+
+    return(gesamte_statistiken)
+}
+# als beispiel können wir untere df betrachen 
+beispiel_daten <- factor(c("Apfel","Apfel","Apfel","Apfel","Apfel", "Banane", "Banane","Banane","Apfel", NA, "Banane","Orange","Orange", NA, "Orange"))
+data <- as.data.frame(beispiel_daten)
+deskriptive_Statistiken_kateg(data)
 ###############################################################################
 # 2a) (iii) - Deskrpitve bivariate Stat. (kategorial):
 calculate_bivariate_stats <- function(var1, var2) {
@@ -88,7 +123,62 @@ calculate_bivariate_stats <- function(var1, var2) {
 
 ###############################################################################
 # 2a) (iv) - Deskriptive bivariate Stat. (metrisch - dichotom):
+ist_binär <- function(variable) {
+  length(unique(na.omit(variable))) == 2
+}
 
+# Funktion, um zu prüfen, ob zwei Datensätze die gleiche Anzahl an Beobachtungen haben
+gleiche_länge <- function(datensatz1, datensatz2) {
+  length(datensatz1) == length(datensatz2)
+}
+
+# Funktion zur Berechnung der Punkt-biserialen Korrelation
+punkt_biseriale_korrelation <- function(kontinuierlich, binär, korrektur = TRUE) {
+  # Prüfungen der Eingabedaten
+  if (!is.numeric(kontinuierlich)) {
+    stop("Die kontinuierliche Variable sollte numerisch sein.")
+  }
+  if (!ist_binär(binär)) {
+    stop("Die binäre Variable sollte genau zwei eindeutige Werte haben.")
+  }
+  if (!gleiche_länge(kontinuierlich, binär)) {
+    stop("Die Variablen sollten die gleiche Anzahl an Beobachtungen haben.")
+  }
+
+  # Fehlende Werte ausschließen
+  vollständig <- complete.cases(kontinuierlich, binär)
+  kontinuierlich <- kontinuierlich[vollständig]
+  binär <- binär[vollständig]
+
+  # Umwandlung der binären Variable in numerisch für die Berechnung
+  binär_numerisch <- as.numeric(as.factor(binär)) - 1
+
+  # verdecken der binären Variablen
+  verst0 <- binär_numerisch == 0
+  verst1 <- binär_numerisch == 1
+
+  # Anzahl der Beobachtungen für jede Gruppe berechnen
+  anzahl0 <- sum(verst0)
+  anzahl1 <- sum(verst1)
+  gesamtanzahl <- anzahl0 + anzahl1
+
+  # Mittelwerte und Standardabweichungen berechnen
+  mittelwert1 <- mean(kontinuierlich[verst0], na.rm = TRUE)
+  mittelwert2 <- mean(kontinuierlich[verst1], na.rm = TRUE)
+  gesamt_mittelwert <- mean(kontinuierlich)
+  gesamt_sd <- sd(kontinuierlich)
+
+  # Punkt-biseriale Korrelation berechnen
+  punkt_korr <- (mittelwert2 - mittelwert1) / gesamt_sd * sqrt((anzahl0 * anzahl1) / gesamtanzahl^2)
+
+  return(punkt_korr)
+}
+
+# Beispielaufruf der Funktion
+ beispiel_daten <- data.frame(KontinuierlicheVariable = rnorm(100), 
+                              BinäreVariable = factor(sample(c("Kategorie1", "Kategorie2"), 100, replace = TRUE)))
+ korrelation <- punkt_biseriale_korrelation(beispiel_daten$KontinuierlicheVariable, beispiel_daten$BinäreVariable)
+ print(korrelation)
 ###############################################################################
 # 2a) (v) - Visualisierung (kategorial):
 visualize_data = function(dataList){
