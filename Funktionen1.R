df1d3a203f2e739df3f1c5761c41af14adafdd4c
Sergen <sergenb.09@gmail.com>
2024-02-24 16:08:05 +0100

    Aktualisierung von Funktionen1.R
    Dokumentation zu A2a) (v) hinzugefügt.

--- Funktionen1.R
+++ Funktionen1.R
@@ -48,40 +48,41 @@ deskriptive_Statistiken(data)
 # 2a) (ii) - Deskriptive Stat. (kategorial):
 deskriptive_Statistiken_kateg = function (daten)
 {
-    # Überprüfen, ob daten ein DataFrame ist 
-    if (!is.data.frame(daten))
-    {
-        stop(  "Die übergebenen Daten sind kein DataFrame.")
-    }
-    # Überprüfen, ob der DataFrame mindestens eine kategoriale Variable (Faktor) enthält
-    if  (!any(sapply(daten, is.factor)))
-    {
-        stop("Der DataFrame enthält keine kategoriale Variable.")
-    }
-
-      # Selektieren aller kategorialen Variablen
-    kategoriale_variablen <- daten[, sapply(daten, is.factor)]
-
-    # Zusammenführen aller kategorialen Variablen in eine einzige Vektor
-    gesamte_kategorien <- unlist(kategoriale_variablen, use.names = FALSE)
-
-    # Berechnen der Gesamthäufigkeiten und relativen Häufigkeiten
-    abs_ha_gesamt <- table(gesamte_kategorien, useNA = "always")
-    re_ha_gesamt <- prop.table(abs_ha_gesamt)
-
-    # Erstellen eines DataFrames für die gesamten Statistiken
-    gesamte_statistiken <- data.frame(
-        Kategorie = names(abs_ha_gesamt),
-        Abs_ha = as.integer(abs_ha_gesamt),
-        Re_ha = re_ha_gesamt
-    )
-
-    return(gesamte_statistiken)
+  # Überprüfen, ob daten ein DataFrame ist 
+  if (!is.data.frame(daten))
+  {
+    stop(  "Die übergebenen Daten sind kein DataFrame.")
+  }
+  # Überprüfen, ob der DataFrame mindestens eine kategoriale Variable (Faktor) enthält
+  if  (!any(sapply(daten, is.factor)))
+  {
+    stop("Der DataFrame enthält keine kategoriale Variable.")
+  }
+  
+  # Selektieren aller kategorialen Variablen
+  kategoriale_variablen <- daten[, sapply(daten, is.factor)]
+  
+  # Zusammenführen aller kategorialen Variablen in eine einzige Vektor
+  gesamte_kategorien <- unlist(kategoriale_variablen, use.names = FALSE)
+  
+  # Berechnen der Gesamthäufigkeiten und relativen Häufigkeiten
+  abs_ha_gesamt <- table(gesamte_kategorien, useNA = "always")
+  re_ha_gesamt <- prop.table(abs_ha_gesamt)
+  
+  # Erstellen eines DataFrames für die gesamten Statistiken
+  gesamte_statistiken <- data.frame(
+    Kategorie = names(abs_ha_gesamt),
+    Abs_ha = as.integer(abs_ha_gesamt),
+    Re_ha = re_ha_gesamt
+  )
+  
+  return(gesamte_statistiken)
 }
 # als beispiel können wir untere df betrachen 
 beispiel_daten <- factor(c("Apfel","Apfel","Apfel","Apfel","Apfel", "Banane", "Banane","Banane","Apfel", NA, "Banane","Orange","Orange", NA, "Orange"))
 data <- as.data.frame(beispiel_daten)
 deskriptive_Statistiken_kateg(data)
+
 ###############################################################################
 # 2a) (iii) - Deskrpitve bivariate Stat. (kategorial):
 calculate_bivariate_stats <- function(var1, var2) {
@@ -144,43 +145,63 @@ punkt_biseriale_korrelation <- function(kontinuierlich, binär, korrektur = TRUE
   if (!gleiche_länge(kontinuierlich, binär)) {
     stop("Die Variablen sollten die gleiche Anzahl an Beobachtungen haben.")
   }
-
+  
   # Fehlende Werte ausschließen
   vollständig <- complete.cases(kontinuierlich, binär)
   kontinuierlich <- kontinuierlich[vollständig]
   binär <- binär[vollständig]
-
+  
   # Umwandlung der binären Variable in numerisch für die Berechnung
   binär_numerisch <- as.numeric(as.factor(binär)) - 1
-
+  
   # verdecken der binären Variablen
   verst0 <- binär_numerisch == 0
   verst1 <- binär_numerisch == 1
-
+  
   # Anzahl der Beobachtungen für jede Gruppe berechnen
   anzahl0 <- sum(verst0)
   anzahl1 <- sum(verst1)
   gesamtanzahl <- anzahl0 + anzahl1
-
+  
   # Mittelwerte und Standardabweichungen berechnen
   mittelwert1 <- mean(kontinuierlich[verst0], na.rm = TRUE)
   mittelwert2 <- mean(kontinuierlich[verst1], na.rm = TRUE)
   gesamt_mittelwert <- mean(kontinuierlich)
   gesamt_sd <- sd(kontinuierlich)
-
+  
   # Punkt-biseriale Korrelation berechnen
   punkt_korr <- (mittelwert2 - mittelwert1) / gesamt_sd * sqrt((anzahl0 * anzahl1) / gesamtanzahl^2)
-
+  
   return(punkt_korr)
 }
 
 # Beispielaufruf der Funktion
- beispiel_daten <- data.frame(KontinuierlicheVariable = rnorm(100), 
-                              BinäreVariable = factor(sample(c("Kategorie1", "Kategorie2"), 100, replace = TRUE)))
- korrelation <- punkt_biseriale_korrelation(beispiel_daten$KontinuierlicheVariable, beispiel_daten$BinäreVariable)
- print(korrelation)
+beispiel_daten <- data.frame(KontinuierlicheVariable = rnorm(100), 
+                             BinäreVariable = factor(sample(c("Kategorie1", "Kategorie2"), 100, replace = TRUE)))
+korrelation <- punkt_biseriale_korrelation(beispiel_daten$KontinuierlicheVariable, beispiel_daten$BinäreVariable)
+print(korrelation)
+
 ###############################################################################
 # 2a) (v) - Visualisierung (kategorial):
+
+# visualize_data  - implementiert eine Funktion, die die uebergebenen
+#                   kategoriellen Variablen als Saeulendiagramm visualisiert.
+#
+# Eingabe:
+#   - dataList: eine benannte Liste mit drei oder vier Elementen. Alle
+#               Listenelemente muessen dabei nicht-numerische Vektoren sein,
+#               welche die Auspraegungen der entsprechenden kategoriellen
+#               Variable enthalten. 
+#
+# Ausgabe:
+# Eine Grafik, die drei oder vier Plots bzw. Saeulendiagramme enthaelt, je nach
+# Laenge des "dataList"-Parameters. Jeder Plot visualisiert dabei jeweils die
+# Auspraegungen, bzw. die dazugehoerigen absoluten Haeufigkeiten, einer
+# kategoriellen Variable, die als Listenelement im "dataList"-Parameter
+# enthalten ist, mit einem passenden Saeulendiagramm. Der Titel eines
+# Saeulendiagramms entspricht dabei dem Namen des Listenelement, der den
+# dazugehoerigen Parametervektor der kategoriellen Variable enthaelt.
+
 visualize_data = function(dataList){
   stopifnot(length(dataList) %in% 3:4, !all(sapply(dataList, is.numeric)))
   
@@ -200,8 +221,8 @@ visualize_data = function(dataList){
   par(mfrow = c(1, 1)) # Zuruecksetzen der Einstellung
 }
 
-# Die Bezeichung bzw. der Name eines Listenelements entspricht dem Titel des
-# zugehoerigen Plots, der durch visualize_data() erzeugt wird:
+# Beispielaufruf der Funktion mit vier ausgewaehlten kategoriellen Variabelen
+# des Datensatzes aus "titantic-WA.R":
 argList = list("Ueberleben der Passagiere" = titanic$Survived,
                "Geschlecht der Passagiere" = titanic$Sex,
                "Zustiegshafen" = titanic$Embarked,
